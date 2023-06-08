e9164af1ed923f4ceb45d6ef541c6971a4d99f3b
yudi0576 <129965125+yudi0576@users.noreply.github.com>
2023-06-08 16:24:53 +0700

    Add files via upload

---
+++ notes_card.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class NotesCard extends StatelessWidget {
+  const NotesCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15),
+      padding: const EdgeInsets.all(15),
+      height: 150,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Keamanan data dan jaringan",
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 5),
+          Text(
+            "klik=>Isi Materi",
+            style: const TextStyle(fontSize: 17),
+          ),
+        ],
+      ),
+    );
+  }
+}
