c9ac85f9a2121fe3b4b1b18d1cf607c0129307b8
yudi0576 <129965125+yudi0576@users.noreply.github.com>
2023-06-08 16:23:54 +0700

    Add files via upload

---
+++ add_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class AddScreen extends StatelessWidget {
+  const AddScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      appBar: AppBar(
+        title: const Text('Data Kelompok 2 Client Server'),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 15,
+          left: 15,
+          right: 15,
+          bottom: 80,
+        ),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Judul Tugas',
+                hintStyle: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+              onChanged: (value) {},
+            ),
+            Expanded(
+              child: TextField(
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Materi Tugas Kelompok',
+                  hintStyle: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                ),
+                style: const TextStyle(
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+                onChanged: (value) {},
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.only(
+                    left: 30, right: 30, top: 10, bottom: 10),
+                backgroundColor: Colors.white,
+              ),
+              child: const Text('Pilih Materi',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueGrey,
+                  )),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
